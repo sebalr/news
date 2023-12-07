7b1fff6d2e6e50344e36d6c9af86efe3b164d05d
Sebastian Larrieu <larrieu.sebastian@gmail.com>
2023-12-07 14:13:42 -0300

    initial components setup and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from 'src/app/core/header/header.component';
+import { FooterComponent } from 'src/app/core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ src/app/news/news.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { NewsComponent } from 'src/app/news/news.component';
+
+export const NEW_ROUTES: Routes = [
+  {
+    path: '',
+    component: NewsComponent,
+  },
+]
