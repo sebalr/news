368a564944a804e67168baf41a494c497f05e472
Sebastian Larrieu <larrieu.sebastian@gmail.com>
2023-12-08 23:21:25 -0300

    dynamic header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgComponentOutlet } from '@angular/common';
+import { HeaderService } from 'src/app/core/header/header.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgComponentOutlet],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+
+  constructor(public headerService: HeaderService) { }
 
 }
--- src/app/core/header/header.service.ts
+++ src/app/core/header/header.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Type, signal } from '@angular/core';
+import { FiltersBarComponent } from 'src/app/news/filters-bar/filters-bar.component';
+import { TitleBarComponent } from 'src/app/title-bar/title-bar.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeaderService {
+  private headerMode = signal<{ component: Type<any>, inputs?: Record<string, unknown> }>({ component: FiltersBarComponent });
+  public $headerMode = this.headerMode.asReadonly();
 
-  constructor() { }
+  public setTitleHeaderMode(title: string): void {
+    this.headerMode.set({ component: TitleBarComponent, inputs: { title } });
+  }
+
+  public setFiltersBarMode(): void {
+    this.headerMode.set({ component: FiltersBarComponent });
+  }
 }
