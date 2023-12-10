eeef573e660d7194a5290ff4335ee0daf53a4b9d
Sebastian Larrieu <larrieu.sebastian@gmail.com>
2023-12-10 19:57:24 -0300

    detail container styles

--- src/app/news/article-detail/article-detail.component.ts
+++ src/app/news/article-detail/article-detail.component.ts
@@ -3,20 +3,22 @@ import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { NewsService } from 'src/app/news/news.service';
 import { HeaderService } from 'src/app/core/header/header.service';
 import { DEFAULT_IMAGE } from 'src/app/shared/constants';
+import { PillComponent } from 'src/app/shared/components/pill/pill.component';
 
 @Component({
   selector: 'app-article-detail',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage],
+  imports: [CommonModule, NgOptimizedImage, PillComponent],
   templateUrl: './article-detail.component.html',
   styleUrls: ['./article-detail.component.scss']
 })
 export class ArticleDetailComponent implements OnInit {
   public readonly DEFAULT_IMAGE = DEFAULT_IMAGE;
+  public $selectedArticle = this.newsService.$selectedArticle;
 
-  constructor(public newsService: NewsService, private headerService: HeaderService) { }
+  constructor(private newsService: NewsService, private headerService: HeaderService) { }
 
   public ngOnInit(): void {
-    this.headerService.setTitleHeaderMode(this.newsService.$selectedArticle()?.title ?? 'Error');
+    this.headerService.setTitleHeaderMode(this.$selectedArticle()?.title ?? 'Error');
   }
 }
