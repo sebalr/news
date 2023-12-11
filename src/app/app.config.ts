7641237de0c77a683c4817e56e337071f2ffa9a6
Sebastian Larrieu <larrieu.sebastian@gmail.com>
2023-12-10 22:49:47 -0300

    pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { newsKeyInterceptor } from 'src/app/core/interceptors/news-key.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([newsKeyInterceptor])),]
+    provideHttpClient(withInterceptors([newsKeyInterceptor])),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
